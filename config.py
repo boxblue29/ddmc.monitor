639dbdf2745ba4c5cb411ced014134232ba6f44b
boxblue29 <103324374+boxblue29@users.noreply.github.com>
2022-04-09 23:24:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # 基本配置
-bark_id = ''  # bark app通知id
+bark_id = '624ff79c28d6b300017cc1e8'  # bark app通知id
 duration = 10  # 执行间隔时间秒
 run_type = 2  # 监控类型 1首页监控(已经失效) 2购物车监控
 
 # 首页get参数
 key_word = '当前运力紧张，今天各时段已约满，敬请谅解'
-station_id = ''  # 站点id
+station_id = '5a1e1664916edf414f8f84f9'  # 站点id
 
 # 购物车post参数
 ua = ''
@@ -14,7 +14,7 @@ city_number = '0101'  # ddmc-city-number 0101上海
 device_id = ''  # ddmc-device-id
 longitude = '121.43676'  # ddmc-longitude 上海的经度
 latitude = '31.188311'  # ddmc-latitude 上海的纬度
-uid = ''  # ddmc-uid
+uid = '624ff79c28d6b300017cc1e8'  # ddmc-uid
 api_version = '9.49.2'  # ddmc-api-version
 build_version = '2.82.0'    # ddmc-build-version
-raw_body = ''
+raw_body = 'uid=624ff79c28d6b300017cc1e8&longitude=121.436511'
